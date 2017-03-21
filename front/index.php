7b6cf8981eb9e5b586b16d2c263705265af0f8c8
Andrei@Menthol <bitfabrika@gmail.com>
2017-03-21 21:30:34 +0200

    Added more files

---
+++ front/index.php
@@ -0,0 +1,1 @@
+<?php defined( 'ABSPATH' ) or die( 'HTTP 2017 - Suddenly fairies.' );
--- index.php
+++ index.php
@@ -8,3 +8,12 @@ Author: I ♥ Menthol
 Text Domain: mint
 Domain Path: /languages
 */
+
+if ( is_admin() ) {
+  //include the files used in the admin
+  //like plugin options/settings
+  require_once('admin/index.php');
+} else {
+  //include de files used in the front part
+  require_once('front/index.php');
+}//-if
